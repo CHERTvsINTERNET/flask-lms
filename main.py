39ab84790834ea10426380bc4bce504badec4bee
User <user135151@gmail.com>
2025-02-25 17:09:24 +0300

    not work. only next step

--- main.py
+++ main.py
@@ -9,6 +9,9 @@ def index():
     name_title = 'Заголовок'
     return render_template('index.html', title=name_title)
 
+@app.route('/training/<prof>')
+def greeting(prof):
+    return render_template('tranning.html', work=prof)
 
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
