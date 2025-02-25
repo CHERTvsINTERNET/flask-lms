8d259dfc75b36bf332c940ebb029254c054ce404
User <user135151@gmail.com>
2025-02-25 16:43:27 +0300

    flask-wtf first NOW WORK

--- main.py
+++ main.py
@@ -6,7 +6,8 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template('index.html', **param)
+    name_title = 'Заголовок'
+    return render_template('index.html', title=name_title)
 
 
 if __name__ == '__main__':
